7ed57ee93b9747e80cfde97ce4eb87271888244d
Oguzhan Inan <oguzhan3488@gmail.com>
2017-02-01 19:45:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,38 +2,41 @@ const {
     app,
     BrowserWindow,
     ipcMain
-} = require("electron")
+} = require('electron')
+
+const electron = require('electron')
 
 let win
 
 function createWindow() {
 
+    const screenSize = electron.screen.getPrimaryDisplay().size;
+
+    const _width = 886, _height = 650
     win = new BrowserWindow({
-        width: 886,
-        height: 700,
-        frame: false,
-        minWidth: 920,
-        minHeight: 700,
-        maxWidth: 920,
-        maxHeight: 700,
+        width: _width,
+        height: _height,
+        minWidth: _width,
+        minHeight: _height,
+        maxWidth: _width,
+        maxHeight: _height,
         maximizable: false,
         resizable: false,
-        center: true,
         icon: './assets/img/icon.png'
     })
+
+    if( screenSize )
+        win.setPosition( (screenSize.width  - _width )  / 2,
+                         (screenSize.height - _height ) / 2 )
+    else
+        win.center()
+
+    win.setMenu(null);
 
     win.loadURL(`file://${__dirname}/index.html`)
 
     win.on("closed", () => {
       win = null
-    })
-
-    ipcMain.on("close-app", () => {
-      app.quit()
-    })
-
-    ipcMain.on("minimize-app", () => {
-      BrowserWindow.getFocusedWindow().minimize()
     })
 
 }
