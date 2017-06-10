4bf3631fb7a9910043b05d156b421e44f7e429c9
oguzhaninan <oguzhan3488@gmail.com>
2017-06-10 19:45:42 +0300

    added search in processes

--- src/components/process/Process.js
+++ src/components/process/Process.js
@@ -1,3 +1,10 @@
+import {
+  spawnSync
+} from 'child_process'
+import {
+  showMessage
+} from '../../utils/helpers.js'
+
 import ProcessesList from './ProcessesList'
 
 export default {
@@ -5,62 +12,67 @@ export default {
                 <div class="content">
 						      <slot></slot>
                   <div class="processes">
-                    <table class="stable">
+                  <div class="fl w100">
+                      <h3 class="fl">{{ lang('process') }} ({{ processes.length }})</h3>
+                      <input type="text" v-model="searchString" :placeholder="lang('search') + '...'" />
+                  </div>
+                  <table class="datatable">
+                    <thead>
+                      <tr>
+                        <th @click="sortPid" scope="col" width="80">
+                          PID <i :class="sortAmount(sPid)"></i>
+                        </th>
+                        <th @click="sortCpu" scope="col" width="70">
+                          %CPU <i :class="sortAmount(sCpu)"></i>
+                        </th>
+                        <th @click="sortMem" scope="col" width="80">
+                          %MEM <i :class="sortAmount(sMem)"></i>
+                        </th>
+                        <th @click="sortRss" scope="col" width="80">
+                          RSS <i :class="sortAmount(sRss)"></i>
+                        </th>
+                        <th @click="sortVSize" scope="col" width="80">
+                          VSIZE <i :class="sortAmount(sVSize)"></i>
+                        </th>
+                        <th @click="sortUser" scope="col" width="100">
+                          User <i :class="sortAmount(sUser)"></i>
+                        </th>
+                        <th @click="sortCmd" scope="col">
+                          Process <i :class="sortAmount(sCmd)"></i>
+                        </th>
+                      </tr>
+                    </thead>
+                  </table>
+                  <div class="tbody scroll">
+                    <table class="datatable" style="margin-top: -18px;">
                       <thead>
                         <tr>
-                          <th @click="sortPid" scope="col" width="80">
-                            PID <i :class="sortAmount(sPid)"></i>
-                          </th>
-                          <th @click="sortCpu" scope="col" width="70">
-                            %CPU <i :class="sortAmount(sCpu)"></i>
-                          </th>
-                          <th @click="sortMem" scope="col" width="80">
-                            %MEM <i :class="sortAmount(sMem)"></i>
-                          </th>
-                          <th @click="sortRss" scope="col" width="80">
-                            RSS <i :class="sortAmount(sRss)"></i>
-                          </th>
-                          <th @click="sortVSize" scope="col" width="80">
-                            VSIZE <i :class="sortAmount(sVSize)"></i>
-                          </th>
-                          <th @click="sortUser" scope="col" width="100">
-                            User <i :class="sortAmount(sUser)"></i>
-                          </th>
-                          <th @click="sortCmd" scope="col">
-                            Process <i :class="sortAmount(sCmd)"></i>
-                          </th>
+                          <th scope="col" width="80"></th>
+                          <th scope="col" width="70"></th>
+                          <th scope="col" width="80"></th>
+                          <th scope="col" width="80"></th>
+                          <th scope="col" width="80"></th>
+                          <th scope="col" width="100"></th>
+                          <th scope="col"></th>
                         </tr>
                       </thead>
+                      <tbody>
+                      <tr v-for="p in processes" @click="selectPid(p.pid)" :class="(selectedPid == p.pid) ? 'selectedRow' : ' '">
+                          <td :title="p.pid">{{ p.pid }}</th>
+                          <td :title="p.pcpu">{{ p.pcpu }}</td>
+                          <td :title="p.pmem">{{ p.pmem }}</td>
+                          <td :title="p.rss">{{ p.rss }}</td>
+                          <td :title="p.vsize">{{ p.vsize }}</td>
+                          <td :title="p.uname">{{ p.uname }}</td>
+                          <td :title="p.cmd">{{ p.cmd }}</td>
+                      </tr>
+                      </tbody>
                     </table>
-                    <div class="tbody scroll">
-                      <table class="stable" style="margin-top: -18px;">
-                        <thead>
-                          <tr>
-                            <th scope="col" width="80"></th>
-                            <th scope="col" width="70"></th>
-                            <th scope="col" width="80"></th>
-                            <th scope="col" width="80"></th>
-                            <th scope="col" width="80"></th>
-                            <th scope="col" width="100"></th>
-                            <th scope="col"></th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                        <tr v-for="p in processes">
-                            <td :title="p.pid">{{ p.pid }}</th>
-                            <td :title="p.pcpu">{{ p.pcpu }}</td>
-                            <td :title="p.pmem">{{ p.pmem }}</td>
-                            <td :title="p.rss">{{ p.rss }}</td>
-                            <td :title="p.vsize">{{ p.vsize }}</td>
-                            <td :title="p.uname">{{ p.uname }}</td>
-                            <td :title="p.cmd">{{ p.cmd }}</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
                   </div>
+                  <button @click="endProcess" :disabled="selectedPid == 0">{{ lang('endProcess') }}</button>
                 </div>
-              </transition>`,
+              </div>
+            </transition>`,
   data() {
     return ({
       processes: [],
@@ -71,13 +83,21 @@ export default {
       sUser: 0,
       sCmd: 0,
       sCpu: 1,
-      sort: ['--sort', '-pcpu']
+      sort: ['--sort', '-pcpu'],
+      searchString: '',
+      selectedPid: 0
     })
   },
   created() {
-    setInterval(() => this.processes = ProcessesList(this.sort), 1000)
+    setInterval(() => this.processes = ProcessesList(this.searchString, this.sort), 1000)
   },
   methods: {
+    selectPid(pid) {
+      if (this.selectedPid == pid)
+        this.selectedPid = 0
+      else
+        this.selectedPid = pid
+    },
     sortPid() {
       this.sPid = this.changeAmount(this.sPid)
       this.changeSort(this.sPid, 'pid')
@@ -120,8 +140,19 @@ export default {
       this.sort = column == 0 ? [] : ['--sort', column == 1 ? '-' + header : header]
     },
     changeAmount(column) {
-        this.sPid = this.sMem = this.sRss = this.sVSize = this.sUser = this.sCmd = this.sCpu = 0
-        return column == 1 ? -1 : (column == 0 ? 1 : 0)
+      this.sPid = this.sMem = this.sRss = this.sVSize = this.sUser = this.sCmd = this.sCpu = 0
+      return column == 1 ? -1 : (column == 0 ? 1 : 0)
+    },
+    endProcess() {
+      if (this.selectedPid != 0) {
+        var kill = spawnSync('kill', [this.selectedPid])
+
+        if (kill.stderr.toString().indexOf('permitted') !== -1)
+          showMessage(lang('permitted'), 'error')
+        else
+          logger.error('Process Kill', kill.stderr.toString())
+      }
+      this.selectedPid = 0
     }
   }
 }
--- src/components/process/ProcessesList.js
+++ src/components/process/ProcessesList.js
@@ -1,23 +1,30 @@
 import {
   spawnSync
 } from 'child_process'
-import fs from 'fs'
 import {
   formatBytes
 } from '../../utils/helpers'
+import fs from 'fs'
 
-export default (sort = []) => {
+export default (search = '', sort = []) => {
+  search = search.toLowerCase()
 
   let ps = spawnSync('ps', ['ax', '-o', 'pid,rss,pmem,vsize,uname,pcpu,cmd', ...sort, '--no-headings'])
 
   if (ps.stderr.toString())
     logger.error('List Processes', ps.stderr.toString())
-  
-  return ps.stdout.toString().split('\n').map(p => procParse(p.trim())).slice(0, -1)
+
+  if ( search )
+    return ps.stdout.toString().split('\n')
+      .filter(p => p.toLowerCase().indexOf(search) !== -1)
+      .map(p => procParse(p.trim())).slice(0, -1)
+  else
+    return ps.stdout.toString().split('\n')
+      .map(p => procParse(p.trim())).slice(0, -1)
 }
 
 function procParse(data) {
-  var columns = data.split(/\s+/)
+  let columns = data.split(/\s+/)
 
   return {
     pid: columns[0],
