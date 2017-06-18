401578fab5453ace2e336bc80171f9fdf6e35c9f
liberodark <liberodark@gmail.com>
2017-06-18 06:00:05 +0200

    Add French

--- src/languages/lang-list.js
+++ src/languages/lang-list.js
@@ -1,5 +1,6 @@
 export default {
 	"en": "English",
+	"fr": "French",
 	"tr": "Turkish",
 	"pt": "Portuguese",
 }
---
+++ src/languages/langs/lang.fr.js
@@ -0,0 +1,80 @@
+/**
+  AUTHOR <email@address>
+*/
+export default {
+	// Top Menu
+	dashboard: "Tableau de bord",
+	sysCleaner: "System Cleaner",
+	startupApps: "Apps de démarrage",
+	services: "Services",
+	process: "Processus",
+	uninstaller: "Désinstallateur",
+	resources: "Resources",
+	// Dashboard Page
+	cpu: "CPU",
+	memory: "MÉMOIRE",
+	disk: "DISQUE",
+	download: "TÉLÉCHARGEMENT",
+	upload: "ENVOIS",
+	systemInfo: "SYSTÈME INFO",
+	hostname: "Hostname",
+	platform: "Plate-forme",
+	distribution: "Distribution",
+	kernelRel: "Kernel Release",
+	cpuModel: "Cpu Model",
+	cpuSpeed: "Cpu Speed",
+	cpuCores: "Cpu Cores",
+	// System Cleaner Page
+	packageCache: "Package Cache",
+	crashReports: "Crash Reports",
+	systemLogs: "Système Logs",
+	appCache: "App Cache",
+	trash: "Corbeille",
+	systemScan: "Analyse du système",
+	clean: "Nettoyer",
+	selectAll: "Tout Sélectionner",
+	// Startup Apps Page
+	startupTitle: "System Startup Applications",
+	addStartup: "Add Startup App",
+	noStartup: "No startup apps found.",
+	app: "Application",
+	appName: "App Name",
+	appComment: "App Comment",
+	appCommand: "Command",
+	add: "Ajouter",
+	cancel: "Annuler",
+	// Services Page
+	serviceTitle: "System Services",
+	noService: "No Service Found",
+	// Process Page
+	endProcess: "End Process",
+	refresh: "Rafraîchir",
+	// Uninstaller Page
+	uninstallerTitle: "System Installed Packages",
+	noPackage: "No package found.",
+	uninstallSelecteds: "Uninstall Selecteds",
+	// Resources Page
+	cpuHistory: "Cpu History",
+	memHistory: "Memory History",
+	networkHistory: "Network History",
+	swap: "Swap",
+	// Settings
+	settings: "Options",
+	appSettings: "Application Settings",
+	appLanguage: "Langue App",
+	appTheme: "Thème App",
+	// Global
+	search: "Chercher",
+	downloadUpdate: "Télécharger la mise à jour",
+	updateMsg: "Il existe actuellement des mises à jour disponibles.",
+	// Messages
+	opFail: "L'opération a échoué.",
+	sysCleanFail: "Le nettoyage du système a échoué.",
+	sysCleanSuc: "Système nettoyé.",
+	appCacheWarning: "Deleting applications\' caches can cause problems. Please be careful.",
+	uninstallFail: "La désinstallation a échoué.",
+	packageUninstalled: " packages uninstalled.",
+	anotherProc: "Another process continues.",
+	changedLang: "Restart the program for the changes to take effect.",
+	permitted: 'Operation not permitted.',
+}
