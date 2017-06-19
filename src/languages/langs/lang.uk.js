3bfb244853e6ea912ed67f1ec0f11e72624d626c
Oguzhan Inan <oguzhan3488@gmail.com>
2017-06-19 10:44:11 +0300

    Added Ukrainian language

---
+++ src/languages/langs/lang.uk.js
@@ -0,0 +1,80 @@
+/**
+  AUTHOR of Ukrainian translation <vpaslavskyi@gmail.com>
+*/
+export default {
+	// Top Menu
+	dashboard: "Панель Приладів",
+	sysCleaner: "Очистка Системи",
+	startupApps: "Початкові Додатки",
+	services: "Сервіси",
+	process: "Процеси",
+	uninstaller: "Видалення Програм",
+	resources: "Ресурси",
+	// Dashboard Page
+	cpu: "ЦП",
+	memory: "ПАМʼЯТЬ",
+	disk: "ДИСК",
+	download: "ЗАВАНТАЖЕННЯ",
+	upload: "ВІДВАНТАЖЕННЯ",
+	systemInfo: "СИСТЕМНА ІНФОРМАЦІЯ",
+	hostname: "Ім'я хосту",
+	platform: "Платформа",
+	distribution: "Дистрибутив",
+	kernelRel: "Випуск ядра",
+	cpuModel: "Модель ЦП",
+	cpuSpeed: "Швидкість ЦП",
+	cpuCores: "Ядра ЦП",
+	// System Cleaner Page
+	packageCache: "Кеш пакунків",
+	crashReports: "Аварійні звіти",
+	systemLogs: "Системні журнали",
+	appCache: "Кеш додатків",
+	trash: "Сміття",
+	systemScan: "Сканування системи",
+	clean: "Очистити",
+	selectAll: "Обрати Все",
+	// Startup Apps Page
+	startupTitle: "Початкові системні додатки",
+	addStartup: "Дадати початковий додаток",
+	noStartup: "Жодних початкових програм не знайдено.",
+	app: "Додатки",
+	appName: "Ім'я додатку",
+	appComment: "Коментар додатку",
+	appCommand: "Команда",
+	add: "Додати",
+	cancel: "Відміна",
+	// Services Page
+	serviceTitle: "Системні Сервіси",
+	noService: "Не знайдено жодного сервісу",
+	// Process Page
+	endProcess: "Кінець Процесу",
+	refresh: "Оновити",
+	// Uninstaller Page
+	uninstallerTitle: "Встановлені Системні Пакунки",
+	noPackage: "Не знайдено жодного пакунку.",
+	uninstallSelecteds: "Видалити Обране",
+	// Resources Page
+	cpuHistory: "Історія ЦП",
+	memHistory: "Історія Пам'яті",
+	networkHistory: "Історія Мережі",
+	swap: "Своп",
+	// Settings 
+	settings: "Налаштування",
+	appSettings: "Налаштування Додатків",
+	appLanguage: "Мова додатку",
+	appTheme: "Тема Додатку",
+	// Global
+	search: "Пошук",
+	downloadUpdate: "Завантажити Оновлення",
+	updateMsg: "Присутні Оновлення Додатку.",
+	// Messages
+	opFail: "Операція не вдалась.",
+	sysCleanFail: "Очищення Системи не вдалось.",
+	sysCleanSuc: "Система очищена.",
+	appCacheWarning: "Киш видалених додатків може призвести до проблем. Будь ласка, будьте обережні.",
+	uninstallFail: "Видалення не вдалось.",
+	packageUninstalled: " пакунки видалені.",
+	anotherProc: "Продовжується інший процес.",
+	changedLang: "Для вступу в дію оновлень, необхідне перезавантаження програми.",
+	permitted: 'Операція не дозволяється.',
+}
