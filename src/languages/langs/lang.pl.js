852d7b78694efa00e6f5148faa1ddb4c1682b85d
AngryPenguinPL <xpris@o2.pl>
2017-06-19 12:11:53 +0200

    Create lang.pl.js

---
+++ src/languages/langs/lang.pl.js
@@ -0,0 +1,81 @@
+@@ -0,0 +1,80 @@
+ +/**
+ +  AUTHOR AngryPenguin <angrypenguinpoland@gmail.com>
+ +*/
+ +export default {
+ +	// Top Menu
+ +	dashboard: "Deska Rozdzielcza",
+ +	sysCleaner: "Czyszczenie Systemu",
+ +	startupApps: "Aplikacje startowe",
+ +	services: "Usługi",
+ +	process: "Procesy",
+ +	uninstaller: "Deinstalator",
+ +	resources: "Zasoby",
+ +	// Dashboard Page
+ +	cpu: "PROCESOR",
+ +	memory: "PAMIĘĆ RAM",
+ +	disk: "DYSK",
+ +	download: "POBIERANIE",
+ +	upload: "WYSYŁANIE",
+ +	systemInfo: "INFORMACJE O SYSTEMIE",
+ +	hostname: "Nazwa hosta",
+ +	platform: "Platforma",
+ +	distribution: "Dystrybucja",
+ +	kernelRel: "Wydanie jądra Linux",
+ +	cpuModel: "Model Cpu",
+ +	cpuSpeed: "Zegar Cpu",
+ +	cpuCores: "Rdzenie Cpu",
+ +	// System Cleaner Page
+ +	packageCache: "Pamięć podręczna pakietów",
+ +	crashReports: "Raporty błędów",
+ +	systemLogs: "Logi systemowe",
+ +	appCache: "Pamięć podręczna aplikacji",
+ +	trash: "Kosz",
+ +	systemScan: "Skanuj System",
+ +	clean: "Wyczyść",
+ +	selectAll: "Zaznacz wszystko",
+ +	// Startup Apps Page
+ +	startupTitle: "Systemowe aplikacje startowe",
+ +	addStartup: "Dodaj aplikację startową",
+ +	noStartup: "Nie znaleziono aplikacji startowych.",
+ +	app: "Aplikacje",
+ +	appName: "Nazwa aplikacji",
+ +	appComment: "Komentarz do aplikacji",
+ +	appCommand: "Polecenie",
+ +	add: "Dodaj",
+ +	cancel: "Anuluj",
+ +	// Services Page
+ +	serviceTitle: "Usługi Systemowe",
+ +	noService: "Nie znaleziono usług",
+ +	// Process Page
+ +	endProcess: "Zakończ proces",
+ +	refresh: "Odśwież",
+ +	// Uninstaller Page
+ +	uninstallerTitle: "Zainstalowane pakiety w systemie",
+ +	noPackage: "Nie znaleziono pakietu.",
+ +	uninstallSelecteds: "Odinstaluj zaznaczone",
+ +	// Resources Page
+ +	cpuHistory: "Historia użycia Cpu",
+ +	memHistory: "Historia użycia pamięci",
+ +	networkHistory: "Historia użycia internetu",
+ +	swap: "Swap",
+ +	// Settings 
+ +	settings: "Ustawienia",
+ +	appSettings: "Ustawienia aplikacji",
+ +	appLanguage: "Język aplikacji",
+ +	appTheme: "Motyw aplikacji",
+ +	// Global
+ +	search: "Wyszukaj",
+ +	downloadUpdate: "Pobierz aktualizację",
+ +	updateMsg: "Są dostępne nowe aktualizacje.",
+ +	// Messages
+ +	opFail: "Operacja zakończona niepowodzeniem.",
+ +	sysCleanFail: "Czyszczenie systemu nieudane.",
+ +	sysCleanSuc: "System oczyszczony.",
+ +	appCacheWarning: "Usuwanie aplikacji\' pamięci podręcznej może powodować problemy. Proszę bądź ostrożny.",
+ +	uninstallFail: "Usuwanie nieudane.",
+ +	packageUninstalled: " pakiety usunięte.",
+ +	anotherProc: "Kolejny proces trwa.",
+ +	changedLang: "Uruchom ponownie program aby wprowadzone zmiany odniosły efekt.",
+ +	permitted: 'Operacja niedozwolona.',
+ +}
