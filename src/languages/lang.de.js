81c91b6b60caa9f8c2aca7e3e4938bd5fd33dae7
Adrian Feld <adrian@feldorado.de>
2017-08-09 14:44:32 +0200

    Create lang.de.js
    First Step for German Language Support

---
+++ src/languages/lang.de.js
@@ -0,0 +1,81 @@
+/**
+  Adrian Feld <adrian@feldorado.de>
+*/
+export default {
+	// Top Menu
+	dashboard: "Dashboard",
+	sysCleaner: "System Cleaner",
+	startupApps: "Startup Apps",
+	services: "Services",
+	process: "Processes",
+	uninstaller: "Uninstaller",
+	resources: "Resources",
+	// Dashboard Page
+	cpu: "CPU",
+	memory: "SPEICHER",
+	disk: "DISK",
+	download: "DOWNLOAD",
+	upload: "UPLOAD",
+	systemInfo: "SYSTEM INFO",
+	hostname: "Hostname",
+	platform: "Platform",
+	distribution: "Distribution",
+	kernelRel: "Kernel Release",
+	cpuModel: "CPU Model",
+	cpuSpeed: "CPU Speed",
+	cpuCores: "CPU Cores",
+	// System Cleaner Page
+	packageCache: "Package Cache",
+	crashReports: "Crash Reports",
+	systemLogs: "System Logs",
+	appCache: "App Cache",
+	trash: "Trash",
+	systemScan: "System Scan",
+	clean: "Säubern",
+	selectAll: "Alles auswählen",
+	// Startup Apps Page
+	startupTitle: "System Startup Applikationen",
+	addStartup: "Hinzufügen Autostart",
+	noStartup: "Keine Autostart Programme gefunden.",
+	app: "Applikation",
+	appName: "App Name",
+	appComment: "App Comment",
+	appCommand: "Command",
+	add: "Add",
+	cancel: "Cancel",
+	// Services Page
+	serviceTitle: "System Services",
+	noService: "Kein Service gefunden",
+	// Process Page
+	endProcess: "End Process",
+	refresh: "Refresh",
+	// Uninstaller Page
+	uninstallerTitle: "System Installed Packages",
+	noPackage: "No package found.",
+	uninstallSelecteds: "Uninstall Selecteds",
+	// Resources Page
+	cpuHistory: "CPU History",
+	memHistory: "Memory History",
+	networkHistory: "Network History",
+	swap: "Swap",
+	// Settings 
+	settings: "Settings",
+	appSettings: "Application Settings",
+	appLanguage: "App Language",
+	appTheme: "App Theme",
+	// Global
+	search: "Search",
+	downloadUpdate: "Download Update",
+	updateMsg: "There are updates currently available.",
+	// Messages
+	opFail: "Operation failed.",
+	sysCleanFail: "System reingung fehlgeschlagen.",
+	sysCleanSuc: "System gereinigt.",
+	appCacheWarning: "Lösche Applikationen \' caches can cause problems. Please be careful.",
+	uninstallFail: "Uninstall failed.",
+	packageUninstalled: " packages uninstalled.",
+	anotherProc: "Another process continues.",
+	changedLang: "Restart the program for the changes to take effect.",
+	permitted: 'Operation not permitted.',
+	update: 'Update'
+}
