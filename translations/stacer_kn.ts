b852061326819fefe51566112ec00b565c88a2d0
Vinyas N S <vinyas.n.s@gmail.com>
2017-08-25 22:07:22 +0530

    Added Kannada Language

---
+++ translations/stacer_kn.ts
@@ -0,0 +1,453 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hi_IN">
+<context>
+    <name>App</name>
+    <message>
+        <location filename="../stacer/app.cpp" line="80"/>
+        <source>Dashboard</source>
+        <translation>ಡ್ಯಾಶ್ಬೋರ್ಡ್</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="85"/>
+        <source>System Cleaner</source>
+        <translation>ಸಿಸ್ಟಮ್ ಕ್ಲೀನರ್</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="90"/>
+        <source>System Startup Apps</source>
+        <translation>ಆರಂಭಿಕ ಅಪ್ಲಿಕೇಶನ್ಗಳು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="95"/>
+        <source>System Services</source>
+        <translation>ಸೇವೆಗಳು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="100"/>
+        <source>Uninstaller</source>
+        <translation>ಅಸ್ಥಾಪನೆಗಾರ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="105"/>
+        <source>Resources</source>
+        <translation>ಸಂಪನ್ಮೂಲಗಳು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="110"/>
+        <source>Processes</source>
+        <translation>ಪ್ರಕ್ರಿಯೆಗಳು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="115"/>
+        <source>Settings</source>
+        <translation>ಸೆಟ್ಟಿಂಗ್ಗಳು</translation>
+    </message>
+</context>
+<context>
+    <name>DashboardPage</name>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.ui" line="153"/>
+        <source>SYSTEM INFO</source>
+        <translation>ಸಿಸ್ಟಮ್ ಮಾಹಿತಿ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.ui" line="240"/>
+        <source>There are update currently available.</source>
+        <translation>ನವೀಕರಣಗಳು ಪ್ರಸ್ತುತ ಲಭ್ಯವಿವೆ.</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.ui" line="253"/>
+        <source>Download</source>
+        <translation>ಡೌನ್‌ಲೋಡ್</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="18"/>
+        <source>CPU</source>
+        <translation>ಕೇಂದ್ರ ಸಂಸ್ಕರಣ ಘಟಕ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="19"/>
+        <source>MEMORY</source>
+        <translation>ಸ್ಮೃತಿ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="20"/>
+        <source>DISK</source>
+        <translation>ಡಿಸ್ಕ್</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="21"/>
+        <source>DOWNLOAD</source>
+        <translation>ಡೌನ್‌ಲೋಡ್</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="22"/>
+        <source>UPLOAD</source>
+        <translation>ಅಪ್‌ಲೋಡ್</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="107"/>
+        <source>Hostname: %1</source>
+        <translation>ಹೋಸ್ಟ್ ಹೆಸರು: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="108"/>
+        <source>Platform: %1</source>
+        <translation>ವೇದಿಕೆ: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="109"/>
+        <source>Distribution: %1</source>
+        <translation>ವಿತರಣೆ: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="110"/>
+        <source>Kernel Release: %1</source>
+        <translation>ಕರ್ನಲ್ ಬಿಡುಗಡೆ: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="111"/>
+        <source>CPU Model: %1</source>
+        <translation>ಸಿಪಿಯು ಮಾದರಿ: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="112"/>
+        <source>CPU Speed: %1</source>
+        <translation>ಸಿಪಿಯು ವೇಗ: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="113"/>
+        <source>CPU Core: %1</source>
+        <translation>ಸಿಪಿಯು ಕೋರ್ಗಳು: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="182"/>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="186"/>
+        <source>Total: %1</source>
+        <translation>ಒಟ್ಟು: %1</translation>
+    </message>
+</context>
+<context>
+    <name>ProcessesPage</name>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.ui" line="55"/>
+        <source>Processes</source>
+        <translation>ಪ್ರಕ್ರಿಯೆಗಳು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.ui" line="68"/>
+        <source>All Processes</source>
+        <translation>ಎಲ್ಲಾ ಪ್ರಕ್ರಿಯೆಗಳು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.ui" line="88"/>
+        <source>Search...</source>
+        <translation>ಹುಡುಕು...</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.ui" line="206"/>
+        <source>End Process</source>
+        <translation>ಪ್ರಕ್ರಿಯೆ ಕೊನೆಗೊಳಿಸಿ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="35"/>
+        <source>Resident Memory</source>
+        <translation>ನಿವಾಸ ಸ್ಮರಣೆ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="35"/>
+        <source>%Memory</source>
+        <translation>%ಸ್ಮೃತಿ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="35"/>
+        <source>Virtual Memory</source>
+        <translation>ವರ್ಚುವಲ್ ಸ್ಮೃತಿ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="35"/>
+        <source>User</source>
+        <translation>ಬಳಕೆದಾರ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>Start Time</source>
+        <translation>ಪ್ರಾರಂಭ ಸಮಯ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>State</source>
+        <translation>ಸ್ಥಿತಿ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>ಗುಂಪು</source>
+        <translation>समूह</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>Nice</source>
+        <translation>ನೈಸ್</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>CPU Time</source>
+        <translation>ಸಿಪಿಯು ಸಮಯ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="37"/>
+        <source>Session</source>
+        <translation>ಅವಧಿ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="37"/>
+        <source>Seat</source>
+        <translation>ಸೀಟ್</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="37"/>
+        <source>Process</source>
+        <translation>ಪ್ರಕ್ರಿಯೆ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="104"/>
+        <source>Processes (%1)</source>
+        <translation>ಪ್ರಕ್ರಿಯೆಗಳು (%1)</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="198"/>
+        <source>Refresh (%1)</source>
+        <translation>ರಿಫ್ರೆಶ್ (%1)</translation>
+    </message>
+</context>
+<context>
+    <name>ResourcesPage</name>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="26"/>
+        <source>CPU History</source>
+        <translation>ಸಿಪಿಯು ಇತಿಹಾಸ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="29"/>
+        <source>Memory History</source>
+        <translation>ಸ್ಮೃತಿ ಇತಿಹಾಸ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="32"/>
+        <source>Network History</source>
+        <translation>ನೆಟ್ವರ್ಕ್ ಇತಿಹಾಸ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="76"/>
+        <source>Download %1/s Total: %2</source>
+        <translation>ಡೌನ್‌ಲೋಡ್ %1/s कुल: %2</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="81"/>
+        <source>Upload %1/s  Total: %2</source>
+        <translation>ಅಪ್‌ಲೋಡ್ %1/s  कुल: %2</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="122"/>
+        <source>Swap %1 (%2%) %3</source>
+        <translation>ವಿನಿಮಯ %1 (%2%) %3</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="131"/>
+        <source>Memory %1 (%2%) %3</source>
+        <translation>ಸ್ಮೃತಿ %1 (%2%) %3</translation>
+    </message>
+</context>
+<context>
+    <name>ServicesPage</name>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.ui" line="79"/>
+        <source>System Services</source>
+        <translation>ಸಿಸ್ಟಮ್ ಸೇವೆಗಳು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.ui" line="123"/>
+        <source>Startup at boot ?</source>
+        <translation>ಬೂಟ್ ಸಮಯದಲ್ಲಿ ಪ್ರಾರಂಭಿಸುವುದೇ??</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.ui" line="176"/>
+        <source>Running Now ?</source>
+        <translation>ಈಗ ಚಾಲನೆಯಲ್ಲಿದೆಯೆ?</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.ui" line="246"/>
+        <source>Not Found System Service</source>
+        <translation>ಯಾವುದೇ ಸಿಸ್ಟಮ್ ಸೇವೆ ಕಂಡುಬಂದಿಲ್ಲ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.cpp" line="50"/>
+        <source>System Services (%1)</source>
+        <translation>ಸಿಸ್ಟಮ್ ಸೇವೆಗಳು (%1)</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../stacer/Pages/Settings/settings_page.ui" line="54"/>
+        <source>Language</source>
+        <translation>ಭಾಷೆ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Settings/settings_page.ui" line="71"/>
+        <source>Theme</source>
+        <translation>ಥೀಮ್</translation>
+    </message>
+</context>
+<context>
+    <name>StartupApp</name>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app.cpp" line="23"/>
+        <source>Delete</source>
+        <translation>ಅಳಿಸಿ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app.cpp" line="24"/>
+        <source>Edit</source>
+        <translation>ತಿದ್ದು</translation>
+    </message>
+</context>
+<context>
+    <name>StartupAppEdit</name>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="20"/>
+        <source>Startup App</source>
+        <translation>ಸಿಸ್ಟಮ್ ಆರಂಭಿಕ ಅಪ್ಲಿಕೇಶನ್</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="50"/>
+        <source>Save</source>
+        <translation>ಉಳಿಸಿ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="60"/>
+        <source>Fields cannot be left blank. </source>
+        <translation>ಕ್ಷೇತ್ರಗಳನ್ನು ಖಾಲಿ ಬಿಡಲಾಗುವುದಿಲ್ಲ. </translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="67"/>
+        <source>App Comment</source>
+        <translation>ऐप टिप्पणी</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="74"/>
+        <source>App Name</source>
+        <translation>ಅಪ್ಲಿಕೇಶನ್ ಕಾಮೆಂಟ್</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="81"/>
+        <source>Command</source>
+        <translation>ಆದೇಶ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="88"/>
+        <source>Application</source>
+        <translation>ಅಪ್ಲಿಕೇಶನ್</translation>
+    </message>
+</context>
+<context>
+    <name>StartupAppsPage</name>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_apps_page.ui" line="95"/>
+        <source>Add Startup App</source>
+        <translation>ಆರಂಭಿಕ ಅಪ್ಲಿಕೇಶನ್ ಸೇರಿಸಿ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_apps_page.ui" line="112"/>
+        <source>System Startup Applications</source>
+        <translation>ಸಿಸ್ಟಮ್ ಆರಂಭಿಕ ಅಪ್ಲಿಕೇಶನ್ಗಳು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_apps_page.ui" line="192"/>
+        <source>Not Found Startup Apps</source>
+        <translation>ಆರಂಭಿಕ ಅಪ್ಲಿಕೇಶನ್ಗಳು ಕಂಡುಬಂದಿಲ್ಲ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_apps_page.cpp" line="79"/>
+        <source>System Startup Applications (%1)</source>
+        <translation>ಸಿಸ್ಟಮ್ ಆರಂಭಿಕ ಅಪ್ಲಿಕೇಶನ್ಗಳು (%1)</translation>
+    </message>
+</context>
+<context>
+    <name>SystemCleanerPage</name>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="63"/>
+        <source>Crash Reports</source>
+        <translation>ಕ್ರ್ಯಾಶ್ ವರದಿಗಳು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="168"/>
+        <source>Application Logs</source>
+        <translation>ಅಪ್ಲಿಕೇಶನ್ ದಾಖಲೆಗಳು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="206"/>
+        <source>Application Caches</source>
+        <translation>ಅಪ್ಲಿಕೇಶನ್ ಸಂಗ್ರಹ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="327"/>
+        <source>Trash</source>
+        <translation>ಅನುಪಯುಕ್ತ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="365"/>
+        <source>Package Caches</source>
+        <translation>ಪ್ಯಾಕೇಜ್ ಸಂಗ್ರಹ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="473"/>
+        <source> Back</source>
+        <translation> ಹಿಂದೆ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.cpp" line="32"/>
+        <source>File Name</source>
+        <translation>ಫೈಲ್ ಹೆಸರು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.cpp" line="32"/>
+        <source>Size</source>
+        <translation>ಗಾತ್ರ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.cpp" line="285"/>
+        <source>%1 size files cleaned.</source>
+        <translation>%1 ಗಾತ್ರದ ಫೈಲ್ಗಳನ್ನು ಸ್ವಚ್ಛಗೊಳಿಸಲಾಗಿದೆ.</translation>
+    </message>
+</context>
+<context>
+    <name>UninstallerPage</name>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="132"/>
+        <source>Uninstall Selected</source>
+        <translation>ಆಯ್ಕೆಮಾಡಿದನ್ನು ಅಸ್ಥಾಪಿಸು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="178"/>
+        <source>Not Found Installed Packages</source>
+        <translation>ಸ್ಥಾಪಿಸಲಾದ ಪ್ಯಾಕೇಜುಗಳು ಕಂಡುಬಂದಿಲ್ಲ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="228"/>
+        <source>System Installed Packages</source>
+        <translation>ಸಿಸ್ಟಮ್ ಅನುಸ್ಥಾಪಿಸಲಾದ ಪ್ಯಾಕೇಜುಗಳು</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="271"/>
+        <source>Search...</source>
+        <translation>ಹುಡುಕು...</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstaller_page.cpp" line="65"/>
+        <source>System Installed Packages (%1)</source>
+        <translation>ಸಿಸ್ಟಮ್ ಅನುಸ್ಥಾಪಿಸಲಾದ ಪ್ಯಾಕೇಜುಗಳು (%1)</translation>
+    </message>
+</context>
+</TS>
