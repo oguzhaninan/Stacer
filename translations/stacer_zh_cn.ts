6ff718ba57450fa9d5de4ac32bedfd5f2b6f42cb
shkey <shkey325@gmail.com>
2017-09-08 19:17:26 +0800

    add translation
    add a translation of Simplified Chinese

---
+++ translations/stacer_zh_cn.ts
@@ -0,0 +1,453 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>App</name>
+    <message>
+        <location filename="../stacer/app.cpp" line="80"/>
+        <source>Dashboard</source>
+        <translation>仪表盘</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="85"/>
+        <source>System Cleaner</source>
+        <translation>系统清理</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="90"/>
+        <source>System Startup Apps</source>
+        <translation>系统开机自启应用</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="95"/>
+        <source>System Services</source>
+        <translation>系统服务</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="100"/>
+        <source>Uninstaller</source>
+        <translation>卸载程序</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="105"/>
+        <source>Resources</source>
+        <translation>资源</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="110"/>
+        <source>Processes</source>
+        <translation>进程</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="115"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+</context>
+<context>
+    <name>DashboardPage</name>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.ui" line="153"/>
+        <source>SYSTEM INFO</source>
+        <translation>仪表盘信息</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.ui" line="240"/>
+        <source>There are update currently available.</source>
+        <translation>当前有可用更新</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.ui" line="253"/>
+        <source>Download</source>
+        <translation>下载</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="18"/>
+        <source>CPU</source>
+        <translation>CPU</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="19"/>
+        <source>MEMORY</source>
+        <translation>内存</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="20"/>
+        <source>DISK</source>
+        <translation>磁盘</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="21"/>
+        <source>DOWNLOAD</source>
+        <translation>下载</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="22"/>
+        <source>UPLOAD</source>
+        <translation>上传</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="107"/>
+        <source>Hostname: %1</source>
+        <translation>主机名: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="108"/>
+        <source>Platform: %1</source>
+        <translation>平台: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="109"/>
+        <source>Distribution: %1</source>
+        <translation>发行版: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="110"/>
+        <source>Kernel Release: %1</source>
+        <translation>内核版本: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="111"/>
+        <source>CPU Model: %1</source>
+        <translation>CPU型号: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="112"/>
+        <source>CPU Speed: %1</source>
+        <translation>CPU频率: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="113"/>
+        <source>CPU Core: %1</source>
+        <translation>CPU核心: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="182"/>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="186"/>
+        <source>Total: %1</source>
+        <translation>总计: %1</translation>
+    </message>
+</context>
+<context>
+    <name>ProcessesPage</name>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.ui" line="55"/>
+        <source>Processes</source>
+        <translation>进程</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.ui" line="68"/>
+        <source>All Processes</source>
+        <translation>所有进程</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.ui" line="88"/>
+        <source>Search...</source>
+        <translation>搜索...</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.ui" line="206"/>
+        <source>End Process</source>
+        <translation>结束进程</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="35"/>
+        <source>User</source>
+        <translation>用户</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="35"/>
+        <source>Resident Memory</source>
+        <translation>常驻内存</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="35"/>
+        <source>%Memory</source>
+        <translation>%内存</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="35"/>
+        <source>Virtual Memory</source>
+        <translation>虚拟内存</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>Start Time</source>
+        <translation>开始时间</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>State</source>
+        <translation>状态</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>Group</source>
+        <translation>组</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>Nice</source>
+        <translation>好</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>CPU Time</source>
+        <translation>CPU 时间</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="37"/>
+        <source>Session</source>
+        <translation>会话</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="37"/>
+        <source>Seat</source>
+        <translation>位置</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="37"/>
+        <source>Process</source>
+        <translation>进程</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="104"/>
+        <source>Processes (%1)</source>
+        <translation>进程 (%1)</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="198"/>
+        <source>Refresh (%1)</source>
+        <translation>刷新 (%1)</translation>
+    </message>
+</context>
+<context>
+    <name>ResourcesPage</name>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="26"/>
+        <source>CPU History</source>
+        <translation>CPU历史</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="29"/>
+        <source>Memory History</source>
+        <translation>内存历史</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="32"/>
+        <source>Network History</source>
+        <translation>网络历史</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="76"/>
+        <source>Download %1/s Total: %2</source>
+        <translation>下载 %1/s 总计: %2</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="81"/>
+        <source>Upload %1/s  Total: %2</source>
+        <translation>上传 %1/s 总计: %2</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="122"/>
+        <source>Swap %1 (%2%) %3</source>
+        <translation>交换 %1 (%2%) %3</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="131"/>
+        <source>Memory %1 (%2%) %3</source>
+        <translation>内存 %1 (%2%) %3</translation>
+    </message>
+</context>
+<context>
+    <name>ServicesPage</name>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.ui" line="79"/>
+        <source>System Services</source>
+        <translation>系统服务</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.ui" line="123"/>
+        <source>Startup at boot ?</source>
+        <translation>开机自启 ?</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.ui" line="176"/>
+        <source>Running Now ?</source>
+        <translation>现在运行 ?</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.ui" line="246"/>
+        <source>Not Found System Service</source>
+        <translation>未找到系统服务</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.cpp" line="50"/>
+        <source>System Services (%1)</source>
+        <translation>系统服务 (%1)</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../stacer/Pages/Settings/settings_page.ui" line="54"/>
+        <source>Language</source>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Settings/settings_page.ui" line="71"/>
+        <source>Theme</source>
+        <translation>主题</translation>
+    </message>
+</context>
+<context>
+    <name>StartupApp</name>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app.cpp" line="23"/>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app.cpp" line="24"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+</context>
+<context>
+    <name>StartupAppEdit</name>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="20"/>
+        <source>Startup App</source>
+        <translation>自启应用程序</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="50"/>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="60"/>
+        <source>Fields cannot be left blank. </source>
+        <translation>字段不能留空 </translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="67"/>
+        <source>App Comment</source>
+        <translation>应用注释</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="74"/>
+        <source>App Name</source>
+        <translation>应用名</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="81"/>
+        <source>Command</source>
+        <translation>命令</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="88"/>
+        <source>Application</source>
+        <translation>应用</translation>
+    </message>
+</context>
+<context>
+    <name>StartupAppsPage</name>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_apps_page.ui" line="192"/>
+        <source>Not Found Startup Apps</source>
+        <translation>未发现自启应用</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_apps_page.ui" line="112"/>
+        <source>System Startup Applications</source>
+        <translation>系统自启应用</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_apps_page.ui" line="95"/>
+        <source>Add Startup App</source>
+        <translation>添加自启应用</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_apps_page.cpp" line="79"/>
+        <source>System Startup Applications (%1)</source>
+        <translation>系统自启 应用 (%1)</translation>
+    </message>
+</context>
+<context>
+    <name>SystemCleanerPage</name>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="63"/>
+        <source>Crash Reports</source>
+        <translation>奔溃报告</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="168"/>
+        <source>Application Logs</source>
+        <translation>应用日志</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="206"/>
+        <source>Application Caches</source>
+        <translation>应用缓存</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="327"/>
+        <source>Trash</source>
+        <translation>垃圾</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="365"/>
+        <source>Package Caches</source>
+        <translation>包缓存</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="473"/>
+        <source> Back</source>
+        <translation> 返回</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.cpp" line="32"/>
+        <source>File Name</source>
+        <translation>文件名</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.cpp" line="32"/>
+        <source>Size</source>
+        <translation>大小</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.cpp" line="285"/>
+        <source>%1 size files cleaned.</source>
+        <translation>%1 大小的文件被清理了</translation>
+    </message>
+</context>
+<context>
+    <name>UninstallerPage</name>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="228"/>
+        <source>System Installed Packages</source>
+        <translation>系统已安装过的包</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="271"/>
+        <source>Search...</source>
+        <translation>搜索...</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="178"/>
+        <source>Not Found Installed Packages</source>
+        <translation>未发现已安装过的包</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="132"/>
+        <source>Uninstall Selected</source>
+        <translation>卸载选中项</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstaller_page.cpp" line="65"/>
+        <source>System Installed Packages (%1)</source>
+        <translation>系统已安装了的包 (%1)</translation>
+    </message>
+</context>
+</TS>
