b1034af3e15b3ce65aeed0658a47bb4a9dc32b00
Oguzhan INAN <oguzhan3488@gmail.com>
2018-03-10 23:22:00 +0300

    updated the release steps

--- release.sh
+++ release.sh
@@ -1,23 +1,33 @@
+#!/bin/bash
 VERSION=1.0.9
+RELEASE=Release
 DIR=stacer-$VERSION
 
-mkdir build && cd build
+mkdir $RELEASE
+mkdir build || cd build
 cmake -DCMAKE_BUILD_TYPE=debug -DCMAKE_CXX_COMPILER=g++ -DCMAKE_PREFIX_PATH=$QTDIR/bin ..
 make -j `nproc`
 cd ..
 
-mkdir $DIR/stacer -p
-mv icons applications $DIR
-cp -r build/output/* $DIR/stacer
+mkdir $RELEASE/$DIR/stacer -p
+cp -r icons applications debian $RELEASE/$DIR
+cp -r build/output/* $RELEASE/$DIR/stacer
 
 # translations
 lupdate stacer/stacer.pro -no-obsolete
 lrelease stacer/stacer.pro
-mkdir $DIR/stacer/translations
-cp translations/*.qm $DIR/stacer/translations
+mkdir $RELEASE/$DIR/stacer/translations
+mv translations/*.qm $RELEASE/$DIR/stacer/translations
 
 # linuxdeployqt
 wget -cO lqt "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod +x lqt
 unset QTDIR; unset QT_PLUGIN_PATH; unset LD_LIBRARY_PATH
-./lqt $DIR/stacer/stacer -bundle-non-qt-libs -no-translations
+./lqt $RELEASE/$DIR/stacer/stacer -bundle-non-qt-libs -no-translations
+rm lqt 
+
+if [ $1 = "deb" ]; then
+cd $RELEASE/$DIR
+dh_make --createorig -i -c mit
+debuild --no-lintian -us -uc
+fi
