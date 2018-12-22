4cfb7646c8d0723f4768f74704a984253c2f451b
Matthew Kehrer <matt91.mek@gmail.com>
2018-12-22 15:05:17 -0600

    md

--- stacer/Pages/SystemCleaner/system_cleaner_mediadir.cpp
+++ stacer/Pages/SystemCleaner/system_cleaner_mediadir.cpp
@@ -1,6 +1,13 @@
 #include "system_cleaner_mediadir.h"
 
-SystemCleanerMediaDir::SystemCleanerMediaDir(QObject *parent) : QObject(parent)
+void MediaDirData::set_data(QString& key, QStringList& value)
+{
+    this->directory(&key);
+    this->filters(&value);
+}
+
+SystemCleanerMediaDir::SystemCleanerMediaDir(QObject *parent) : QObject(parent),
+    mContainsAnything(false)
 {
     init();
 }
@@ -31,25 +38,37 @@ pMediaDirectories SystemCleanerMediaDir::mediaDirectories() const
     return &mDirectories;
 }
 
+bool SystemCleanerMediaDir::containsAnything() const
+{
+    return mContainsAnything;
+}
+
 void SystemCleanerMediaDir::init()
 {
-    // the home directory is our default media directory
-    const QString& default_mediadir = QStandardPaths::writableLocation(QStandardPaths::HomeLocation);
-    addDirectory(default_mediadir);
+    // DOES NOTHING
+    addDefaultDirs(false);
 }
 void SystemCleanerMediaDir::addDirectory(const QString& dir)
 {
     mDirectories.insert(dir, QStringList());
+	
+	auto *a = &mDirectories.constFind(dir).key();
+    if(!mContainsAnything)
+        mContainsAnything = true;
+	emit addedMediaDirectory(const_cast<QString*>(a), nullptr);
 }
 
 void SystemCleanerMediaDir::addFilterToDirectory(const QString& filter)
 {
+    QString emitkey;
+    
     if (mDirectories.size() < 2)
     {
         const QString& okey = mDirectories.begin().key();
         QStringList filters = mDirectories.take(okey);
         filters << filter;
         mDirectories.insert(okey, filters);
+        emitkey = QString(okey);
     }
     else
     {
@@ -57,11 +76,33 @@ void SystemCleanerMediaDir::addFilterToDirectory(const QString& filter)
         QStringList filters = mDirectories.take(okey);
         filters << filter;
         mDirectories.insert(okey, filters);
+        emitkey = QString(okey);
     }
+    
+    auto *a = &mDirectories.constFind(emitkey).key();
+    auto *b = &mDirectories.constFind(emitkey).value();
+    if(!mContainsAnything)
+        mContainsAnything = true;
+    emit addedMediaDirectory(const_cast<QString*>(a), const_cast<QStringList*>(b));
 }
 void SystemCleanerMediaDir::addFilterToDirectory(const QString& dir, const QString& filter)
 {
     QStringList filters = mDirectories.take(dir);
     filters << filter;
     mDirectories.insert(dir,filters);
+    
+    auto *a = &mDirectories.constFind(dir).key();
+    auto *b = &mDirectories.constFind(dir).value();
+    if(!mContainsAnything)
+        mContainsAnything = true;
+    emit addedMediaDirectory(const_cast<QString*>(a), const_cast<QStringList*>(b));
+}
+
+void SystemCleanerMediaDir::addDefaultDirs(const bool do_it)
+{
+    if (!do_it)
+        return;
+    
+    const QString& default_mediadir = QStandardPaths::writableLocation(QStandardPaths::HomeLocation);
+    addDirectory(default_mediadir);
 }
--- stacer/Pages/SystemCleaner/system_cleaner_mediadir.h
+++ stacer/Pages/SystemCleaner/system_cleaner_mediadir.h
@@ -9,6 +9,22 @@
 #include <QFileInfoList>
 #include <QStandardPaths>
 
+class MediaDirData {
+public:
+    MediaDirData() = default;
+    virtual ~MediaDirData() { }
+
+    const QString& directory(QString *dir=nullptr) {if(dir){mKey=QString(*dir);} return const_cast<QString&>(mKey);}
+    QString& directory() { return mKey; }
+    const QStringList& filters(QStringList *filt=nullptr) {if(filt){QStringList qsl;std::copy(filt,filt+1,&qsl);mValues.clear();mValues << qsl;} return const_cast<QStringList&>(mValues);}
+    QStringList& filters() { return mValues; }
+
+    virtual void set_data(QString& key, QStringList& value);
+private:
+    QString mKey;
+    QStringList mValues;
+};
+
 typedef QMap<QString, QStringList> MediaDirectories;
 typedef const MediaDirectories* pMediaDirectories;
 
@@ -18,13 +34,19 @@ class SystemCleanerMediaDir : public QObject
 
     Q_PROPERTY(pMediaDirectories mediaDirectories READ mediaDirectories)
 
+    Q_PROPERTY(bool containsAnything MEMBER mContainsAnything READ containsAnything NOTIFY firstDirAdded)
+
 public:
     explicit SystemCleanerMediaDir(QObject *parent = nullptr);
 
     QFileInfoList fetchFIL(void);
 
     pMediaDirectories mediaDirectories() const;
+    bool containsAnything() const;
+
 signals:
+    void   addedMediaDirectory(QString *dir, QStringList *filters);
+    void   firstDirAdded(void);
 
 private slots:
     void init();
@@ -33,9 +55,12 @@ public slots:
     void addDirectory(const QString& dir);
     void addFilterToDirectory(const QString& dir);
     void addFilterToDirectory(const QString& dir, const QString& filter);
+    
+    void addDefaultDirs(const bool do_it=false);
 
 private:
     MediaDirectories mDirectories;
+    bool             mContainsAnything;
 };
 
 #endif // SYSTEM_CLEANER_MEDIADIR_H
