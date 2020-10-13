635d2eb119ae8fcf4a4cd9cf5c117d3dc4326af1
Lilian A. Moraru <lilian.moraru90@gmail.com>
2020-10-13 17:25:11 +0300

    Fix HighDPI support, remove cxxbasics, improve LTO + PIE detection

--- cmake/cxxbasics/DefaultSettings.cmake
+++
@@ -1,9 +0,0 @@
-# This module sets reasonable defaults that probably every C/C++ CMake project should
-
-cmake_minimum_required(VERSION 3.0 FATAL_ERROR)
-
-# Default build type "Debug"
-opt_ifndef("Build Type(Debug, Release, RelWithDebInfo, MinSizeRel)"  STRING  "Debug"  CMAKE_BUILD_TYPE)
-
-# Generate "compile_commands.json" - tools like clang-tidy can be run on this file
-set(CMAKE_EXPORT_COMPILE_COMMANDS  ON)
